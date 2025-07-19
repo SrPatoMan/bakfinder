296f766e52edc6862de717aa245c6b29ab2d7e2c
mramosm15 <manumanram2@gmail.com>
2025-07-19 19:04:10 +0200

    asd

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 
 	target := flag.String("u", "", "URL of your target. Ej: -u https://subdomain.domain.com")
 	target_file := flag.String("l", "", "Target list")
+	concurrent := flag.Int("t", 20, "Amount of threads")
 	flag.Parse()
 
 	if *target == "" && *target_file == "" {
